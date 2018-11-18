223676fd0a1fbd501c0ec1711a3f62b25db1710e
constgen <const.gen@gmail.com>
2018-11-18 23:08:50 +0200

    Redesign tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,58 +1,274 @@
 ﻿import { areIntersected, filterVisible } from '../src/index';
 
-function stringifyRect (rect) {
-	return `[${rect.left},${rect.top}; ${rect.width} x ${rect.height}]`;
-}
 
 describe('The function', function () {
-	let rect1 = Object.freeze({
+	const { freeze } = Object;
+	const mainRect = freeze({
+		left: 0, top: 0,
+		width: 20, height: 20
+	});
+	const intersectedRects = [
+		{
 			left: 0, top: 0,
 			width: 20, height: 20
-		}),
-		rect2 = Object.freeze({
-			left: 10, top: 10,
-			width: 25, height: 25
-		}),
-		rect3 = Object.freeze({
-			left: -10, top: -10,
-			width: 25, height: 25
-		}),
-		rect4 = Object.freeze({
-			left: 0, top: -5,
-			width: 25, height: 0
-		}),
-		rect5 = Object.freeze({
-			left: -20, top: 10,
-			width: 5, height: 5
-		}),
-		rect6 = Object.freeze({
-			left: 0, top: 0,
-			width: 0, height: 0
-		}),
-		rect7 = Object.freeze({
-			left: 100, top: 10,
-			width: 5, height: 5
-		}),
-		rect8 = Object.freeze({
+		},
+		{
 			left: 5, top: 5,
 			width: 10, height: 10
-		}),
-		rect9 = Object.freeze({
+		},
+		{
+			left: -5, top: -5,
+			width: 30, height: 30
+		},
+		{
+			left: -10, top: -10,
+			width: 40, height: 20
+		},
+		{
+			left: 10, top: -10,
+			width: 25, height: 25
+		},
+		{
+			left: 10, top: -10,
+			width: 20, height: 40
+		},
+		{
+			left: 10, top: 10,
+			width: 25, height: 25
+		},
+		{
+			left: -10, top: 10,
+			width: 40, height: 25
+		},
+		{
+			left: -10, top: 10,
+			width: 20, height: 25
+		},
+		{
+			left: -10, top: -10,
+			width: 25, height: 40
+		},
+		{
+			left: -10, top: -10,
+			width: 25, height: 25
+		},
+		{
+			left: 0, top: -10,
+			width: 20, height: 20
+		},
+		{
+			left: 10, top: -10,
+			width: 20, height: 20
+		},
+		{
+			left: 10, top: 0,
+			width: 20, height: 20
+		},
+		{
+			left: 10, top: 10,
+			width: 20, height: 20
+		},
+		{
+			left: 0, top: 10,
+			width: 20, height: 20
+		},
+		{
+			left: -10, top: 10,
+			width: 20, height: 20
+		},
+		{
+			left: -10, top: 0,
+			width: 20, height: 20
+		},
+		{
+			left: -10, top: -10,
+			width: 25, height: 25
+		}
+	];
+	const notIntersectedRects = [
+		{
+			left: -100, top: -100,
+			width: 40, height: 20
+		},
+		{
+			left: -10, top: -100,
+			width: 15, height: 25
+		},
+		{
+			left: -10, top: -100,
+			width: 40, height: 25
+		},
+		{
+			left: 10, top: -100,
+			width: 25, height: 25
+		},
+		{
+			left: 100, top: -100,
+			width: 25, height: 25
+		},
+		{
+			left: 100, top: -10,
+			width: 20, height: 15
+		},
+		{
+			left: 100, top: -10,
+			width: 20, height: 40
+		},
+		{
+			left: 100, top: 10,
+			width: 25, height: 25
+		},
+		{
+			left: 100, top: 100,
+			width: 20, height: 40
+		},
+		{
+			left: 10, top: 100,
+			width: 25, height: 25
+		},
+		{
+			left: -10, top: 100,
+			width: 40, height: 25
+		},
+		{
+			left: -10, top: 100,
+			width: 15, height: 25
+		},
+		{
+			left: -100, top: 100,
+			width: 25, height: 25
+		},
+		{
+			left: -100, top: 10,
+			width: 25, height: 25
+		},
+		{
+			left: -100, top: -10,
+			width: 20, height: 40
+		},
+		{
+			left: -100, top: -10,
+			width: 20, height: 15
+		}
+	];
+	const adjacentRects = [
+		{
+			left: -20, top: -20,
+			width: 20, height: 20
+		},
+		{
+			left: -10, top: -20,
+			width: 20, height: 20
+		},
+		{
+			left: 0, top: -20,
+			width: 20, height: 20
+		},
+		{
+			left: 10, top: -20,
+			width: 20, height: 20
+		},
+		{
+			left: 20, top: -20,
+			width: 20, height: 20
+		},
+		{
+			left: 20, top: -10,
+			width: 20, height: 20
+		},
+		{
+			left: 20, top: 0,
+			width: 20, height: 20
+		},
+		{
+			left: 20, top: 10,
+			width: 20, height: 20
+		},
+		{
 			left: 20, top: 20,
 			width: 20, height: 20
-		}),
-		rect10 = Object.freeze({
-			left: -10, top: 0,
-			width: 10, height: 10
-		}),
-		rect11 = Object.freeze({
-			left: 10, top: 10,
-			width: 0, height: 25
+		},
+		{
+			left: 10, top: 20,
+			width: 20, height: 20
+		},
+		{
+			left: 0, top: 20,
+			width: 20, height: 20
+		},
+		{
+			left: -10, top: 20,
+			width: 20, height: 20
+		},
+		{
+			left: -20, top: 20,
+			width: 20, height: 20
+		},
+		{
+			left: -20, top: 10,
+			width: 20, height: 20
+		},
+		{
+			left: -20, top: 0,
+			width: 20, height: 20
+		},
+		{
+			left: -20, top: -10,
+			width: 20, height: 20
+		}
+	];
+	const zeroRects = [
+		{
+			left: 0, top: 0,
+			width: 20, height: 0
+		},
+		{
+			left: 5, top: 5,
+			width: 0, height: 0
+		},
+		{
+			left: 5, top: 5,
+			width: 10, height: 0
+		},
+		{
+			left: 5, top: 5,
+			width: 0, height: 10
+		}
+	];
+
+	function stringifyRect (rect) {
+		return rect ? `[${rect.left},${rect.top}; ${rect.width} x ${rect.height}]` : '';
+	}
+
+	function stringify (strings, ...rects) {
+		return strings.map(function (string, index) {
+			const rect = rects[index];
+			const rectString = stringifyRect(rect);
+
+			return `${string}${rectString}`;
+		}).join('');
+	}
+
+	function testIntersected ({ rectA, rectB }) {
+		it(stringify`${rectA} and ${rectB}`, function () {
+			expect(areIntersected(rectA, rectB)).toBe(true);
 		});
+	}
+
+	function testNotIntersected ({ rectA, rectB }) {
+		it(stringify`${rectA} and ${rectB}`, function () {
+			expect(areIntersected(rectA, rectB)).toBe(false);
+		});
+	}
+
+	function pairWithMainRect (rect) {
+		return {
+			rectA: rect,
+			rectB: mainRect
+		};
+	}
 
 	describe('`areIntersected`', function () {
-		let intersected;
-
 		it('is defined', function () {
 			expect(areIntersected).toBeDefined();
 		});
@@ -61,85 +277,33 @@ describe('The function', function () {
 			expect(areIntersected).toBeInstanceOf(Function);
 		});
 
-		beforeEach(function () {
-			intersected = undefined;
-		});
-
 		describe('can detect', function () {
-			describe('intersection', function () {
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect2)}`, function () {
-					intersected = areIntersected(rect1, rect2);
-					expect(intersected).toBe(true);
-				});
+			describe('intersected rectangles', function () {
+				intersectedRects
+					.map(freeze)
+					.map(pairWithMainRect)
+					.forEach(testIntersected);
+			});
 
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect3)}`, function () {
-					intersected = areIntersected(rect1, rect3);
-					expect(intersected).toBe(true);
-				});
-
-				it(`${stringifyRect(rect4)} and ${stringifyRect(rect3)}`, function () {
-					intersected = areIntersected(rect4, rect3);
-					expect(intersected).toBe(true);
-				});
-
-				it(`${stringifyRect(rect3)} and ${stringifyRect(rect3)}`, function () {
-					intersected = areIntersected(rect3, rect3);
-					expect(intersected).toBe(true);
-				});
-
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect8)}`, function () {
-					intersected = areIntersected(rect1, rect8);
-					expect(intersected).toBe(true);
-				});
+			describe('intersection with collapsed rectangles', function () {
+				zeroRects
+					.map(freeze)
+					.map(pairWithMainRect)
+					.forEach(testIntersected);
 			});
 
 			describe('not intersected rectangles', function () {
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect5)}`, function () {
-					intersected = areIntersected(rect1, rect5);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect2)} and ${stringifyRect(rect7)}`, function () {
-					intersected = areIntersected(rect2, rect7);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect4)} and ${stringifyRect(rect7)}`, function () {
-					intersected = areIntersected(rect4, rect7);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect2)} and ${stringifyRect(rect5)}`, function () {
-					intersected = areIntersected(rect2, rect5);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect4)}`, function () {
-					intersected = areIntersected(rect1, rect4);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect7)}`, function () {
-					intersected = areIntersected(rect1, rect7);
-					expect(intersected).toBe(false);
-				});
+				notIntersectedRects
+					.map(freeze)
+					.map(pairWithMainRect)
+					.forEach(testNotIntersected);
 			});
 
-			describe('adjacent(surrounding) rectangles are not intersected ', function () {
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect6)}`, function () {
-					intersected = areIntersected(rect1, rect6);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect9)}`, function () {
-					intersected = areIntersected(rect1, rect9);
-					expect(intersected).toBe(false);
-				});
-
-				it(`${stringifyRect(rect1)} and ${stringifyRect(rect10)}`, function () {
-					intersected = areIntersected(rect1, rect10);
-					expect(intersected).toBe(false);
-				});
+			describe('adjacent(surrounding) rectangles are not intersected', function () {
+				adjacentRects
+					.map(freeze)
+					.map(pairWithMainRect)
+					.forEach(testNotIntersected);
 			});
 		});
 	});
@@ -161,55 +325,52 @@ describe('The function', function () {
 
 		describe('can filter', function () {
 			it('an array of rectangles', function () {
-				let parent = rect1;
-				let rectangles = [
-					rect2,
-					rect3,
-					rect5,
-					rect7,
-					rect8,
-					rect9,
-					rect10
+				const rectangles = [
+					intersectedRects[5],
+					intersectedRects[6],
+					notIntersectedRects[6],
+					notIntersectedRects[7],
+					intersectedRects[7],
+					notIntersectedRects[8],
+					notIntersectedRects[9]
 				];
 
-				filtered = filterVisible(parent, rectangles);
+				filtered = filterVisible(mainRect, rectangles);
 				expect(filtered).toEqual([
-					rect2,
-					rect3,
-					rect8
+					intersectedRects[5],
+					intersectedRects[6],
+					intersectedRects[7]
 				]);
 			});
 
 			it('an array of rectangles, where some are invisible', function () {
-				let parent = rect1;
-				let rectangles = [
-					rect2,
-					rect3,
-					rect4,
-					rect5,
-					rect6,
-					rect7,
-					rect8,
-					rect9,
-					rect10,
-					rect11
+				const rectangles = [
+					intersectedRects[2],
+					zeroRects[1],
+					intersectedRects[3],
+					notIntersectedRects[6],
+					notIntersectedRects[7],
+					intersectedRects[4],
+					zeroRects[3],
+					notIntersectedRects[8],
+					notIntersectedRects[9]
 				];
 
-				filtered = filterVisible(parent, rectangles);
+				filtered = filterVisible(mainRect, rectangles);
 				expect(filtered).toEqual([
-					rect2,
-					rect3,
-					rect8
+					intersectedRects[2],
+					intersectedRects[3],
+					intersectedRects[4]
 				]);
 			});
 
 			it('an empty array', function () {
-				filtered = filterVisible(rect1, []);
+				filtered = filterVisible(mainRect, []);
 				expect(filtered).toHaveLength(0);
 			});
 
 			it('no rectangles, if the parent rectangle is invisible', function () {
-				filtered = filterVisible(rect6, []);
+				filtered = filterVisible(mainRect, []);
 				expect(filtered).toHaveLength(0);
 			});
 		});
